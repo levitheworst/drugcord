a859a237e4296af2fbb43b1c4192046d1987bafd
Levi Sales <105523157+levitheworst@users.noreply.github.com>
2024-08-31 13:03:44 -0300

    delete dist folder

--- dist/models/player.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Player {
-    constructor(id, user) {
-        this.id = id;
-        this.user = user;
-    }
-}
-exports.default = Player;
